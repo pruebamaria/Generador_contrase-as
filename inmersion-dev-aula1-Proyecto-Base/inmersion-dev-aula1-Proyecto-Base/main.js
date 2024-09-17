80c6a1dc6487018ac8fec1f332d73ba3acce43f0
María Elisa Muñoz <98441904+pruebamaria@users.noreply.github.com>
2024-09-17 18:20:39 -0500

    Add files via upload
    Gracias a los Instructores e Instructoras de Alura en esta inmersión Christian Velasco - Leonardo Jose Castillo Lacruz - Jeanmarie Quijada aprendi muchicimo y logre realizar el proyecto

---
+++ inmersion-dev-aula1-Proyecto-Base/inmersion-dev-aula1-Proyecto-Base/main.js
@@ -0,0 +1,44 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena');
+
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generar(){
+
+    let numeroDigitado = parseInt (cantidad.value);
+    console.log(numeroDigitado);
+
+
+    if (numeroDigitado < 8){
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+    }
+    let password = '';
+
+    for(let i= 0; i< numeroDigitado; i++  ){
+
+        let caracterAleatorio = cadenaCaracteres [Math.floor (Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
+
+        password+=caracterAleatorio;
+    }
+
+    contrasena.value = password;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
